18e4cdb9c0e5e49ff4c2d4fb535a982ca2f6ecc2
CorleoneYu <364723957@qq.com>
2020-02-26 10:53:53 +0800

    feat: docs image PPT

--- teacher/src/index.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import MonkeyCompilerIDE from './MonkeyCompilerIDE';
-import registerServiceWorker from './registerServiceWorker';
-
-ReactDOM.render(<MonkeyCompilerIDE />, document.getElementById('root'));
-registerServiceWorker();
